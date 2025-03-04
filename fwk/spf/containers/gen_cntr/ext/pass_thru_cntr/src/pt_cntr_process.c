78f45ec06dd112aebf79e6e1c12a21102bf269c5
Sai Raghu Vamsi Anumula <sranumul@qti.qualcomm.com>
2025-03-05 03:49:19 +0530

    fwk: Fix Pass Thru container to correctly set module flags in control path.
    Fix in Pass Thru container to update module level flags correctly in the
    control path.
    
    [Ported to audioreach-engine-auto.cmn.1.0]
    CRs-Fixed: 4380426
    
    Change-Id: I55720726b65560b04f00ab4a56d9b4849c308480

--- fwk/spf/containers/gen_cntr/ext/pass_thru_cntr/src/pt_cntr_process.c
+++ fwk/spf/containers/gen_cntr/ext/pass_thru_cntr/src/pt_cntr_process.c
@@ -45,10 +45,28 @@ PT_CNTR_STATIC void pt_cntr_propagate_ext_output_buffer_backwards(pt_cntr_t     
                                                                   capi_stream_data_v2_t *sdata_ptr,
                                                                   uint32_t               num_bufs_to_update);
 
+static inline uint32_t __pt_cntr_module_flags_has_attached_module_bitmask()
+{
+   pt_cntr_module_flags_t temp_flag;
+   temp_flag.word                       = 0xFFFFFFFF;
+   temp_flag.has_attached_module = 0;
+   return (~temp_flag.word);
+}
+#define  PT_CNTR_MODULE_FLAGS_HAS_ATTACHED_MODULE_BIT_MASK       (__pt_cntr_module_flags_has_attached_module_bitmask())
+
+static inline uint32_t __pt_cntr_module_flags_has_stopped_port_bitmask()
+{
+   pt_cntr_module_flags_t temp_flag;
+   temp_flag.word                       = 0xFFFFFFFF;
+   temp_flag.has_stopped_port = 0;
+   return (~temp_flag.word);
+}
+#define  PT_CNTR_MODULE_FLAGS_HAS_STOPPED_PORT_BIT_MASK       (__pt_cntr_module_flags_has_stopped_port_bitmask())
+
 static inline bool_t pt_cntr_check_if_output_needs_post_process(pt_cntr_module_t *module_ptr)
 {
-   // todo: optimize the check with preprocess offset macros
-   return module_ptr->flags.has_attached_module || module_ptr->flags.has_stopped_port;
+   return module_ptr->flags.word &
+          (PT_CNTR_MODULE_FLAGS_HAS_ATTACHED_MODULE_BIT_MASK | PT_CNTR_MODULE_FLAGS_HAS_STOPPED_PORT_BIT_MASK);
 }
 
 void pt_cntr_propagate_ext_output_buffer_backwards_non_static(pt_cntr_t             *me_ptr,
@@ -2235,7 +2253,22 @@ ar_result_t pt_cntr_signal_trigger(cu_base_t *cu_ptr, uint32_t channel_bit_index
    if (me_ptr->gc.st_module.raised_interrupt_counter > me_ptr->gc.st_module.processed_interrupt_counter)
    {
       // todo: check if anything needs to be done on signal miss ?
-      GEN_CNTR_MSG(me_ptr->gc.topo.gu.log_id, DBG_ERROR_PRIO, "pt_cntr_signal_trigger: Signal Miss");
+      GEN_CNTR_MSG(me_ptr->gc.topo.gu.log_id,
+                   DBG_ERROR_PRIO,
+                   "pt_cntr_signal_trigger: Signal Miss num misses: (Handled IRQs - Unhandled IRQs) %lu - %lu = %lu missed IRQs",
+                   me_ptr->gc.st_module.raised_interrupt_counter,
+                   me_ptr->gc.st_module.processed_interrupt_counter,
+                   me_ptr->gc.st_module.raised_interrupt_counter - me_ptr->gc.st_module.processed_interrupt_counter);
+
+      // signal miss cannot be handled in island, even if signal miss is to be ignored, we will exit island. This
+      // reduces island footprint.
+      // gen_topo_exit_island_temporarily(&me_ptr->gc.topo);
+      // bool_t continue_processing = TRUE;
+      // gen_cntr_check_handle_signal_miss(&me_ptr->gc, FALSE /*is_after_process*/, &continue_processing);
+      // if (!continue_processing)
+      // {
+      //    return result;
+      // }
    }
 
    /*clear the trigger signal */
