8dbe13083dafe057ce109c46a21662ec70f9d663
hahire <hahire@qti.qualcomm.com>
2025-11-06 16:08:54 +0530

    audioreach-engine: Add check for supported PROC_DOMAIN configuration
    Add check in make file to ensure PROC_DOMAIN value is defined,
    non-empty and has a supported processor domain ID value.
    
    Change-Id: Ia5522d2db93026ce1ddc30a120bd3027777a5bcf
    (cherry picked from commit 629a3ea2a9789a7281285ce187fa59526fe10ebd)

--- Android.mk
+++ Android.mk
@@ -7,3 +7,25 @@ GLOBAL_SPF_LIBS_LIST :=
 include $(MY_LOCAL_PATH_ARE)/scripts/make/kconfig_parser.mk
 include $(MY_LOCAL_PATH_ARE)/modules/build/Android.mk
 include $(MY_LOCAL_PATH_ARE)/fwk/build/Android.mk
+
+# read CONFIG_PROC_DOMAIN from .config file
+CONFIG_LINE := $(file < $(PROJECT_BINARY_DIR).config)
+CONFIG_PROC_DOMAIN := $(patsubst CONFIG_PROC_DOMAIN=%,%,$(filter CONFIG_PROC_DOMAIN=%,$(CONFIG_LINE)))
+CONFIG_PROC_DOMAIN := $(subst ",,$(CONFIG_PROC_DOMAIN))
+
+# Check if CONFIG_PROC_DOMAIN is defined and not empty
+ifeq ($(CONFIG_PROC_DOMAIN),)
+    $(error Undefined/Empty CONFIG_PROC_DOMAIN configuration)
+endif
+
+# Define supported PROC_DOMAIN values
+SUPPORTED_PROC_DOMAINS := ADSP MDSP APPS SDSP CDSP CC_DSP GDSP_0 GDSP_1 APPS_2
+
+ifeq ($(filter $(CONFIG_PROC_DOMAIN), $(SUPPORTED_PROC_DOMAINS)),)
+    $(error Unsupported CONFIG_PROC_DOMAIN value: $(CONFIG_PROC_DOMAIN). Supported values are: $(SUPPORTED_PROC_DOMAINS))
+else
+    $(info CONFIG_PROC_DOMAIN value '$(CONFIG_PROC_DOMAIN)' is supported.)
+endif
+
+# Set derived variable
+PROC_DOMAIN_NAME := PROC_DOMAIN_$(CONFIG_PROC_DOMAIN)
